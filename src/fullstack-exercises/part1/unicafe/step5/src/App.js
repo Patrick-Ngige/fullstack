9b9e47271f4f3edead9fd95e95485eb43daebcc2
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-23 19:37:53 +0300

    extracting Button and statisticLine components

--- src/fullstack-exercises/part1/unicafe/step5/src/App.js
+++ src/fullstack-exercises/part1/unicafe/step5/src/App.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
 
+const StatisticLine = (props) => {
+  return (
+    <>
+      <p>
+        {props.text} : {props.value}
+      </p>
+    </>
+  );
+};
+
 const Statistics = (props) => {
   if (props.total > 0) {
     return (
       <>
         <h1>Statistics</h1>
-        <p>good: {props.good}</p>
-        <p>neutral: {props.neutral}</p>
-        <p>bad: {props.bad}</p>
-        <p>total: {props.total}</p>
-        <p>avereage: {props.average}</p>
-        <p>positive: {props.positive} %</p>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="total" value={props.total} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={`${props.positive}%`} />
       </>
     );
   }
