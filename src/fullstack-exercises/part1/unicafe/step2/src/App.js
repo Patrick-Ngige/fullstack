36d55fefbb1abc175e89eb44ab18228e17753f02
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-23 17:08:23 +0300

    shows more statistics about the gathered feedback

---
+++ src/fullstack-exercises/part1/unicafe/step2/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avereage, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const handleGoodClick = () => {
+    const updateGood = good + 1
+    const updatedTotal = updateGood + neutral + bad
+    const updatedAverage = updatedTotal / 3
+    const updatedPositive = (updateGood / updatedTotal) * 100
+    setGood(updateGood)
+    setTotal(updatedTotal)
+    setAverage(updatedAverage)
+    setPositive(updatedPositive)
+  } 
+
+  const handleNeutralClick = () => {
+    const updateNeutral = neutral + 1
+    const updatedTotal = good + updateNeutral + bad
+    const updatedAverage = updatedTotal / 3
+    const updatedPositive = (good / updatedTotal) * 100
+    setNeutral(updateNeutral)
+    setTotal(updatedTotal)
+    setAverage(updatedAverage)
+    setPositive(updatedPositive)
+    
+  } 
+
+  const handleBadClick = () => {
+    const updateBad = bad + 1
+    const updatedTotal = good + neutral + updateBad
+    const updatedAverage = updatedTotal / 3
+    const updatedPositive = (good / updatedTotal) * 100
+    setBad(updateBad)
+    setTotal(updatedTotal) 
+    setAverage(updatedAverage)
+    setPositive(updatedPositive)
+ 
+  } 
+
+  
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h1>Statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p >total: {total}</p>
+      <p >avereage: {avereage}</p>
+      <p >positive: {positive} %</p>
+    </>
+  )
+}
+
+export default App
