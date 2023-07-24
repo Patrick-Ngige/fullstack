83ee76d0dc8a35ce5ad780c9f8d16aaa127c7d10
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-24 23:18:23 +0300

    used <tbody> to do away roads

--- src/fullstack-exercises/part1/unicafe/step6/src/App.js
+++ src/fullstack-exercises/part1/unicafe/step6/src/App.js
@@ -15,16 +15,14 @@ const Statistics = (props) => {
       <>
         <h1>Statistics</h1>
         <table>
-          <tr>
-            <td>
+          <tbody>
               <StatisticLine text="good" value={props.good} />
               <StatisticLine text="neutral" value={props.neutral} />
               <StatisticLine text="bad" value={props.bad} />
               <StatisticLine text="total" value={props.total} />
               <StatisticLine text="average" value={props.average} />
               <StatisticLine text="positive" value={`${props.positive}%`} />
-            </td>
-          </tr>
+          </tbody>
         </table>
       </>
     );
