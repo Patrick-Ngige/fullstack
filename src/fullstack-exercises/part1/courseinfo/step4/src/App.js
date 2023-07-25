f28a85ee990c1b605bb90a92c38911ebfcc96a5c
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-25 18:20:17 +0300

    placing the objects into an array.

---
+++ src/fullstack-exercises/part1/courseinfo/step4/src/App.js
@@ -0,0 +1,64 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.m}
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  );
+};
+
+const TotalExercises = (props) => {
+  const totalExercises =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
+  return (
+    <>
+      <p>Total number of exercises = {totalExercises}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <TotalExercises parts={parts} />
+    </div>
+  );
+};
+
+export default App;
